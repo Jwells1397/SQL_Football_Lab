69a693d0cbbd56b6926e74991184422103765724
Jack Wells <jackwellsjw@yahoo.com>
2022-05-10 09:19:14 +0100

    Updated version

--- football_queries.sql
+++ football_queries.sql
@@ -18,16 +18,36 @@ SELECT divisions.name From divisions WHERE Country = 'Scotland';
 --Use that code to find out how many matches Freiburg have played 
 --in the Bundesliga since the data started being collected.
 
-SELECT * FROM matches Where division_code = 'D1' AND (hometeam = 'Freiburg'
-OR awayteam = 'Freiburg');
-
+SELECT code FROM divisions WHERE name = 'Bundesliga';
+SELECT COUNT(*) FROM matches WHERE division_code = 'D1' AND (hometeam = 'Freiburg' OR awayteam = 'Freiburg');
 
 
 --5.Find the unique names of the teams which include the word 
 --"City" in their name (as entered in the database)
 
-SELECT id FROM matches WHERE hometeam LIKE '%City%' OR awayteam LIKE '%City%';
+SELECT DISTINCT hometeam FROM matches WHERE hometeam LIKE '%City%';
 
 --6.How many different teams 
 --have played in matches recorded in a French division?
---SELECT hometeam AND awayteam FROM matches WHERE division_code = 'F1' OR division_code ='F2' GROUP BY hometeam AND awayteam;
+SELECT code FROM divisions WHERE country = 'France';
+
+SELECT COUNT(DISTINCT hometeam) FROM matches WHERE division IN ('F1', 'F2');
+
+--7.Have Huddersfield played Swansea in the period covered?
+SELECT * FROM matches WHERE (hometeam = 'Huddersfield' AND awayteam = 'Swansea') OR (hometeam = 'Swansea' AND awayteam = 'Huddersfield');
+
+--8.How many draws were there in the Eredivisie between 2010 and 2015?
+SELECT code FROM divisions WHERE name = 'Eredivisie';
+
+SELECT COUNT(*) FROM matches WHERE division_code = 'N1' AND ftr = 'D' AND season BETWEEN 2010 AND 2015;
+
+--9.Select the matches played in the Premier League in order of total goals scored from highest to lowest. Where there is a tie the match with more home goals should come first.
+SELECT code FROM divisions WHERE name = 'Premier League';
+
+SELECT * FROM matches WHERE division_code = 'E0' ORDER BY (fthg + ftag) DESC, fthg DESC;
+
+--10.In which division and which season were the most goals scored?
+
+SELECT division_code, season, SUM (fthg + ftag) FROM matches GROUP BY division_code, season ORDER BY sum DESC LIMIT 1;
+
+SELECT name FROM divisions WHERE code = 'EC';
